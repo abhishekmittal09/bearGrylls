250bc34117a0789cea989d938b6ef9674e75b204
Dark Dragon <abhishekmittaliiit@gmail.com>
2016-01-29 14:51:12 +0530

    Working Sample

---
+++ BearGryllsApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BearGryllsApp.MvcApplication" Language="C#" %>
---
+++ BearGryllsApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BearGryllsApp.Startup))]
+namespace BearGryllsApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
